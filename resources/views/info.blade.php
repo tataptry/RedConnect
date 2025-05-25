81ac73b7cab652402f67f36d1abbc1fc8ef54da3
Tata Ptry <marethaputri270@gmail.com>
2025-05-25 15:47:02 +0700

    update

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -4,13 +4,18 @@
 
 @section('content')
 <!-- Info Donor -->
-<!-- Info Donor -->
+<div>
+      <h1 class="text-3xl font-bold text-red-700 text-center">Jadilah Pahlawan, Cukup dengan Donor Darah</h1>
+</div>
 <div class="bg-white shadow-md rounded-xl p-6 border-l-4 border-red-600 flex flex-col justify-between">
   <div>
     <h2 class="text-2xl font-bold text-red-600 mb-4">Info Donor</h2>
     <p class="text-gray-700 mb-4 text-lg leading-relaxed">
-      Donor darah dapat membantu menyelamatkan hingga tiga nyawa. Prosesnya aman, cepat, dan juga bermanfaat bagi kesehatan pendonor.
-      Rutin mendonor dapat menurunkan risiko penyakit jantung, meningkatkan kesehatan tubuh, dan memberikan rasa kebersamaan serta kepedulian terhadap sesama.
+     Apa itu Donor Darah? 
+
+Donor darah adalah kegiatan mendonorkan atau menyumbangkan darah secara sukarela. Donor ini dilakukan untuk memenuhi kebutuhan orang-orang yang memerlukan tambahan darah, seperti penderita trauma berat yang kehilangan banyak darah, anemia, talasemia, hemofilia, hingga leukemia. 
+
+Tujuan donor darah adalah untuk kepentingan sosial dan kemanusiaan. Prosedur ini berada di bawah pengawasan Palang Merah Indonesia (PMI) dan telah dijamin keamanannya dalam UU No. 36 Tahun 2009 tentang Kesehatan.
     </p>
     <p class="text-gray-700 text-lg leading-relaxed">
       Jangan ragu untuk menjadi pahlawan bagi orang lain! Setetes darahmu sangat berarti untuk kehidupan orang lain.
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/tentang-kami', function () {
 });
 
 //Halaman Kebijakan
-Route::get('/kebijakan', function () {
+Route::get('/Kebijakan', function () {
     return view('policy'); 
 });
 
