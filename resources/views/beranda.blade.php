cd7b6f885ea99a5ab98079614a9c3879f71c2d32
Tiajh <tiahenim25@gmail.com>
2025-05-25 16:43:26 +0700

    update

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -3,22 +3,23 @@
 @section('title', 'Beranda - RedConnect')
 
 @section('content')
-    <div class="max-w-7xl mx-auto flex flex-col md:flex-row items-center md:items-start gap-8 py-12 px-6">
-        <!-- Bagian Kiri: Gambar -->
-        <div class="md:w-1/2 flex justify-center">
-          <img src="{{ asset('assets/img/logo.png') }}" alt="Logo Donor Darah Indonesia">
-                 class="rounded-lg shadow-md max-w-[300px] w-full h-auto object-cover" />
-        </div>
-
-        <!-- Bagian Kanan: Teks -->
-        <div class="md:w-1/2 space-y-6">
-            <h1 class="text-4xl font-bold text-red-700">Selamat Datang di RedConnect</h1>
-            <p class="text-gray-700 text-lg leading-relaxed">
-                RedConnect hadir untuk mempermudah proses donor darah dan meningkatkan kesadaran masyarakat tentang pentingnya donor darah.
-                Bergabunglah dengan kami untuk membantu menyelamatkan nyawa dan menyebarkan kebaikan.
-            </p>
-        </div>
+    <div class="max-w-4xl mx-auto flex flex-col md:flex-row items-center md:items-start gap-8 py-12 px-6">
+    <!-- Bagian Kiri: Gambar -->
+    <div class="md:w-1/2 flex justify-center md:justify-start shadow-md w-48 h-20">
+        <img src="{{ asset('assets/img/fix.jpg') }}" alt="Logo Donor Darah Indonesia"
+            class="rounded-lg shadow-md w-48 md:w-64 object-contain" />
     </div>
+
+    <!-- Bagian Kanan: Teks -->
+    <div class="md:w-1/2 space-y-3">
+        <h1 class="text-4xl font-bold text-red-700">Selamat Datang di RedConnect</h1>
+        <p class="text-gray-700 text-lg leading-relaxed">
+            RedConnect hadir untuk mempermudah proses donor darah dan meningkatkan kesadaran masyarakat tentang pentingnya donor darah.
+            Bergabunglah dengan kami untuk membantu menyelamatkan nyawa dan menyebarkan kebaikan. Kamu tidak perlu jubah atau kekuatan super untuk jadi pahlawan. Cukup datang ke lokasi donor, dan berikan sedikit waktumu. Darah yang kamu berikan adalah harapan bagi mereka yang tak kamu kenal.
+        </p>
+    </div>
+</div>
+
 
     <!-- Info Donor & Jadwal Donor -->
     <section class="max-w-6xl mx-auto px-4 py-8">
