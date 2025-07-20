2e95f6bc2fc28babce2b0c175189c44f0601b378
Tiajh <tiahenim25@gmail.com>
2025-07-20 19:12:19 +0700

    7:12

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
-        <h2 class="text-2xl font-bold text-center text-red-600 mb-6">RedConnect</h2>
+        <h2 class="text-2xl font-bold text-center text-red-600 mb-6">Admin RedConnect</h2>
 
         @if ($errors->any())
             <div class="bg-red-100 text-red-700 p-4 rounded mb-4">
@@ -32,19 +32,11 @@
                     class="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 border-gray-300" />
             </div>
 
-            <p class="text-right mt-4">
-            <a href="{{ route('lupapw') }}" class="text-black hover:underline"><u><i>Lupa Password?</i></u></a>
-            </p>
-
             <div>
                 <br><button type="submit"
                     class="w-full mt-2 bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded transition">
                     Masuk
                 </button>
-
-            <p class="text-center mt-4">
-            Belum punya akun? <a href="{{ route('daftar') }}" class="text-blue-600 hover:underline"><u>Daftar</u></a>
-            </p>
 
             </div>
         </form>
